43df6ee47fd47905eb26ac6ef59c13e89609fc88
Oluwaseun <oluwaseunazure@yahoo.com>
2020-06-07 11:39:10 +0100

    api key

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import TypoGraphy from '@material-ui/core/Typography';
+import NavBar from './navbar';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import HospitalSearch from './hospitalsearch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <AppBar color="primary" position="static">
+          <Toolbar>
+            <TypoGraphy variant={"h4"} color="inherit" >
+            Hospital Finder <LocalHospitalIcon fontSize="large"></LocalHospitalIcon>
+            </TypoGraphy> 
+            <NavBar></NavBar>         
+          </Toolbar>
+        </AppBar>
+        <HospitalSearch></HospitalSearch>
+      </div>
   );
 }
 
---
+++ src/hospitalsearch.tsx
@@ -0,0 +1,68 @@
+import React, {FC} from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import DirectionsIcon from '@material-ui/icons/Directions';
+import TextField from '@material-ui/core/TextField';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    root: {
+      padding: '2px 4px',
+      display: 'flex',
+      alignItems: 'center',
+      width: 400,
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+  }),
+);
+
+const HospitalSearch: FC = () => 
+    const classes = useStyles();
+    const [address, setAddress] = React.useState("");
+    const handleSelect = async (value: any) => {};
+
+    
+        <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: 20,
+          padding: 20
+        }}>
+      <Paper component="form" className={classes.root}>
+        <IconButton className={classes.iconButton} aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelect}>
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading}) => (<InputBase
+          className={classes.input}
+          {...getInputProps({placeholder: "Search Hospitals Near You"})}          
+          inputProps={{ 'aria-label': 'search hospitals near you' }}
+        />)}        
+        </PlacesAutocomplete>
+        <IconButton type="submit" className={classes.iconButton} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <Divider className={classes.divider} orientation="vertical" />
+        <IconButton color="primary" className={classes.iconButton} aria-label="directions">
+          <DirectionsIcon />
+        </IconButton>
+      </Paper>
+      </div>
+    
+  }
